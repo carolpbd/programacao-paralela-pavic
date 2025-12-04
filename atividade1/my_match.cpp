7221b09876651f44289596ba37db3504447a1b80
Carol Bandeira <43106712+carolpbd@users.noreply.github.com>
2025-12-04 17:23:01 -0500

    Delete atividade1 directory

--- atividade1/my_match.cpp
+++
@@ -1,5 +0,0 @@
-#include "my_math.h"
-
-double reciprical(double d) {
-    return 1.0 / d;
-}
--- atividade1/my_math.h
+++
@@ -1,6 +0,0 @@
-#ifndef H_MY_MATH
-#define H_MY_MATH
-
-double reciprical(double d);
-
-#endif // H_MY_MATH
