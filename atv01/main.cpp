b211c1dcac420cfa497cc7c56999d252f835dd3a
Carol Bandeira <43106712+carolpbd@users.noreply.github.com>
2025-12-04 17:23:52 -0500

    Add files via upload

---
+++ atv01/main.cpp
@@ -0,0 +1,16 @@
+#include "my_math.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    reciprocal(2.0);
+    
+    cout << reciprocal(2.0) << endl;
+
+
+    system("pause");
+    
+    return 0;
+
+}
---
+++ atv01/my_math.cpp
@@ -0,0 +1,11 @@
+#include "my_math.h"
+
+double reciprocal(double d) {
+    
+    if (d == 0.0) {
+        return 0.0; 
+    }
+    
+    
+    return 1.0 / d;
+}
